bea6a80d91ff550956b7733f04cdd4045d95c275
Amanda McCumber <Serenity333@users.noreply.github.com>
2015-05-24 13:44:54 -0400

    Create highslide-with-gallery.js

---
+++ highslide/highslide-with-gallery.js
@@ -0,0 +1,32 @@
+hs.graphicsDir = 'highslide/graphics/';
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.75;
+hs.fadeInOut = true;
+hs.align = 'center';
+hs.useBox = true;
+hs.width = 600;
+hs.height = 400;
+hs.captionEval = 'this.a.title';
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	fixedControls: 'fit',
+	overlayOptions: {
+		opacity: 1,
+		position: 'bottom center',
+		offsetX: 0,
+		offsetY: -15,
+		hideOnMouseOut: true
+	}
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	transitions: ['expand', 'crossfade']
+}
